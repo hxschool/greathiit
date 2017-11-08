dc41285c322bab7a8b070de0a7158eb7e1f3b27c
zhaojunfei <773152@qq.com>
2017-11-08 16:58:10 +0800

    新增字典参数配置

---
+++ src/main/java/com/thinkgem/jeesite/modules/out/system/web/SystemDictController.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.thinkgem.jeesite.modules.out.system.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.thinkgem.jeesite.common.config.Global;
+import com.thinkgem.jeesite.common.utils.StringUtils;
+import com.thinkgem.jeesite.common.web.BaseController;
+import com.thinkgem.jeesite.modules.sys.entity.Dict;
+import com.thinkgem.jeesite.modules.sys.service.DictService;
+
+/**
+ * 字典Controller
+ * @author ThinkGem
+ * @version 2014-05-16
+ */
+@Controller
+@RequestMapping(value = "${adminPath}/out/system/dict")
+public class SystemDictController extends BaseController {
+
+	@Autowired
+	private DictService dictService;
+	
+	@ModelAttribute
+	public Dict get(@RequestParam(required=false) String id) {
+		if (StringUtils.isNotBlank(id)){
+			return dictService.get(id);
+		}else{
+			return new Dict();
+		}
+	}
+
+
+	
+	@RequestMapping(value = "form")
+	public String form(Dict dict, Model model) {
+		model.addAttribute("dict", dict);
+		return "modules/out/dict/dictForm";
+	}
+
+	@RequestMapping(value = "save")//@Valid 
+	public String save(Dict dict, Model model, RedirectAttributes redirectAttributes) {
+		if(Global.isDemoMode()){
+			addMessage(redirectAttributes, "演示模式，不允许操作！");
+			return "redirect:" + adminPath + "/sys/dict/?repage&type="+dict.getType();
+		}
+		if (!beanValidator(model, dict)){
+			return form(dict, model);
+		}
+		dictService.save(dict);
+		addMessage(redirectAttributes, "保存字典'" + dict.getLabel() + "'成功");
+		return "redirect:" + adminPath + "/out/system/dict/form?id="+dict.getId();
+	}
+	
+	
+
+}

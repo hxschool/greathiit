a4507bb0d436b9fadfdf5c6115b240a2491e1062
zhaojunfei <773152@qq.com>
2017-11-08 17:28:15 +0800

    修复代码不更新bug

--- src/main/java/com/thinkgem/jeesite/modules/sys/service/DictService.java
+++ src/main/java/com/thinkgem/jeesite/modules/sys/service/DictService.java
@@ -36,8 +36,8 @@ public class DictService extends CrudService<DictDao, Dict> {
 	public void save(Dict dict) {
 		if(dict.getType().equals("greathiit_zhaosheng")&&dict.getValue().equals("2018")){
 			dict.setDescription(StringEscapeUtils.unescapeHtml4(dict.getDescription()));
-			super.save(dict);
 		}
+		super.save(dict);
 		CacheUtils.remove(DictUtils.CACHE_DICT_MAP);
 	}
 

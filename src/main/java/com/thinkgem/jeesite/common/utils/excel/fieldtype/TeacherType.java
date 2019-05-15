04de4f2f04eeee81fcd505bef6ac0cddc4f16cce
Administrator <Administrator@windows10.microdone.cn>
2019-05-15 13:04:53 +0800

    新增导出模式

---
+++ src/main/java/com/thinkgem/jeesite/common/utils/excel/fieldtype/TeacherType.java
@@ -0,0 +1,41 @@
+package com.thinkgem.jeesite.common.utils.excel.fieldtype;
+
+import java.util.List;
+
+import com.thinkgem.jeesite.common.utils.Collections3;
+import com.thinkgem.jeesite.common.utils.SpringContextHolder;
+import com.thinkgem.jeesite.common.utils.StringUtils;
+import com.thinkgem.jeesite.modules.teacher.entity.Teacher;
+import com.thinkgem.jeesite.modules.teacher.service.TeacherService;
+
+public class TeacherType {
+	private static TeacherService teacherService = SpringContextHolder.getBean(TeacherService.class);
+
+	/**
+	 * 获取对象值（导入）
+	 */
+	public static Object getValue(String val) {
+		
+		List<Teacher> allTeacherList = teacherService.findList(new Teacher());
+		for (String s : StringUtils.split(val, ",")) {
+			for (Teacher e : allTeacherList) {
+				if (StringUtils.trimToEmpty(s).equals(e.getTchrName())) {
+					return e;
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 设置对象值（导出）
+	 */
+	public static String setValue(Object val) {
+		if (val != null) {
+			@SuppressWarnings("unchecked")
+			List<Teacher> teacherList = (List<Teacher>) val;
+			return Collections3.extractToString(teacherList, "tchrName", ", ");
+		}
+		return "";
+	}
+}

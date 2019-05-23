5d49ebe20402874d37f9ffa4345c64964ebe0d52
Administrator <Administrator@ieonline.microsoft.com>
2019-05-23 11:09:34 +0800

    修复isadmin

--- src/main/java/com/thinkgem/jeesite/modules/sys/entity/User.java
+++ src/main/java/com/thinkgem/jeesite/modules/sys/entity/User.java
@@ -352,11 +352,6 @@ public class User extends DataEntity<User> {
 	}
 	
 	public boolean isAdmin(){
-		for(Role role:getRoleList()) {
-			if(role.getDataScope().equals("1")||role.getDataScope().equals("2")||role.getRoleType().equals("security-role")) {
-				return true;
-			}
-		}
 		return isAdmin(this.id);
 	}
 	
